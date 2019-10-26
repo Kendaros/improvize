d5b0abadaf86b8b933749f09691f432e22873b91
Jacky Fong <jacky.fong@afp.com>
2019-10-26 15:39:19 +0200

    Add external links to documentation and issue-tracking

---
+++ pages/resources/documentation/index.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  fetch ({ redirect }) {
+    redirect(301, 'https://docs.thefreecat.org/')
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ pages/resources/issue-tracking/index.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  fetch ({ redirect }) {
+    redirect(301, 'https://mantis.thefreecat.org/')
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
