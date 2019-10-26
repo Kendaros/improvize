bdfecfd9037f8f90a22a8ebe07787e7b141c4ff5
Jacky Fong <jacky.fong@afp.com>
2019-10-26 21:40:43 +0200

    Working NavBar with Submenu

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,18 +5,25 @@
         v-for="menuItem in groups"
         :key="menuItem.title"
         class="menu-item"
-        @mouseover="showSubmenu(menuItem.links)"
-        @mouseout="hideSubmenu"
+        @mouseenter="toggleSubmenu(menuItem.title)"
+        @mouseleave="toggleSubmenu('')"
       >
         {{ $t(`menu.${menuItem.title}`) }}
+        <div
+          v-if="activeCategory === menuItem.title"
+          class="submenu"
+        >
+          <n-link
+            v-for="submenuItem in menuItem.links"
+            :key="submenuItem"
+            :to="{ name: `${menuItem.title}-${submenuItem}___${$route.name.slice(-2)}` }"
+            class="link"
+          >
+            <sub-item />
+          </n-link>
+        </div>
       </li>
     </ul>
-    <div class="submenu">
-      <sub-item
-        v-for="submenuItem in submenu"
-        :key="submenuItem"
-      />
-    </div>
   </nav>
 </template>
 
@@ -30,20 +37,14 @@ import { Category } from '~/utils/interfaces'
   components: { SubItem }
 })
 export default class NavBar extends Vue {
-  submenu: Array<string> = []
+  activeCategory: string = ''
 
   get groups (): Array<Category> {
     return config.menu
   }
 
-  showSubmenu (submenuItems: Array<string>) {
-    if (submenuItems) {
-      this.submenu = submenuItems
-    }
-  }
-
-  hideSubmenu () {
-    this.submenu = []
+  toggleSubmenu (category: string) {
+    this.activeCategory = category || ''
   }
 }
 </script>
@@ -73,6 +74,11 @@ export default class NavBar extends Vue {
     box-shadow: 0 2px 10px 0 rgba(0, 0, 0, 0.11);
     padding: 0 22px;
     background: #FFFFFF;
+
+    .link {
+      color: $font_dark_color;
+      text-decoration: none;
+    }
   }
 }
 </style>
