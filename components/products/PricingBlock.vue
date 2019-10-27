57e6a7e73b0d451034859ac7b7bd81d3f034ff6b
Jacky Fong <jacky.fong@afp.com>
2019-10-27 02:13:45 +0100

    Add technical assistance button

--- components/products/PricingBlock.vue
+++ components/products/PricingBlock.vue
@@ -3,15 +3,23 @@
     <h2 class="title">
       Pricing
     </h2>
-    <div class="pricings">
-      <pricing-block-item
-        v-for="(item, i) in pricing"
-        :key="`pricing-block-${i}`"
-        :title="$t(`products.${productName}.pricing.${item.title}.title`)"
-        :price="item.price"
-        :features="$t(`products.${productName}.pricing.${item.title}.features`)"
-        :button="item.button"
-      />
+    <div class="pricings-and-link">
+      <div class="pricings">
+        <pricing-block-item
+          v-for="(item, i) in pricing"
+          :key="`pricing-block-${i}`"
+          :title="$t(`products.${productName}.pricing.${item.title}.title`)"
+          :price="item.price"
+          :features="$t(`products.${productName}.pricing.${item.title}.features`)"
+          :button="item.button"
+        />
+      </div>
+      <n-link
+        :to="{ name: `services-support___${$route.name.slice(-2)}` }"
+        class="technical-assistance"
+      >
+        {{ $t('general.technical-assistance') }}
+      </n-link>
     </div>
   </section>
 </template>
@@ -36,23 +44,41 @@ export default class PricingBlock extends Vue {
 .pricing {
   background-image: linear-gradient(to bottom, rgba(248, 246, 255, 0), rgba(75, 121, 234, 0.06));
   padding: 145px 0;
+  text-align: center;
 
   > .title {
     text-align: center;
   }
 
-  > .pricings {
-    display: flex;
-    justify-content: center;
+  > .pricings-and-link {
     margin-top: 38px;
+    display: inline-flex;
+    flex-direction: column;
 
-    .pricing-block-item {
-      + .pricing-block-item {
-        margin-left: 28px;
+    > .pricings {
+      display: flex;
+      justify-content: center;
+
+      .pricing-block-item {
+        + .pricing-block-item {
+          margin-left: 28px;
+        }
+
+        &:first-child {
+          box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);
+        }
       }
+    }
 
-      &:first-child {
-        box-shadow: 0 2px 15px 0 rgba(0, 0, 0, 0.1);
+    > .technical-assistance {
+      margin-top: 20px;
+      margin-left: auto;
+      align-self: flex-end;
+      color: $contrast_color;
+      font-weight: bold;
+
+      &:hover {
+        cursor: pointer;
       }
     }
   }
